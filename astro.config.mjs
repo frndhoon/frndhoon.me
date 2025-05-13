107c7dfdc51774cefdac6b64a836a229eb4ccefe
frndhoon <frndhoon@gmail.com>
2025-05-13 13:46:42 +0900

    chore: 절대 경로 설정 및 TypeScript, VSCode 설정 업데이트

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   },
 
   integrations: [svelte()],

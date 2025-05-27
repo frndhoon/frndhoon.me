add400e5c8717e6128ffd46efd9bd23cab387662
frndhoon <frndhoon@gmail.com>
2025-05-27 17:01:35 +0900

    feat: UI 컴포넌트 통합을 위한 인덱스 파일 추가

---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { ProjectCard } from './project-card';
+export { ProjectDetailModal } from './project-detail-modal';
+export { ProjectList } from './project-list';
+export { TechStack } from './tech-stack';
